97d07a3b2b90728510084720b4ae6b2323fe81c8
Oyku <han.oyku@gmail.com>
2020-02-15 22:55:20 -0500

    custom train/valid dataloaders for resnet18

--- src/data/dataloader.py
+++ src/data/dataloader.py
@@ -1,10 +1,11 @@
 import random
 from torch.utils.data import DataLoader, random_split
-from matplotlib.pyplot import imshow
 import numpy as np
 from PIL import Image
+import torchvision
 
-from dataset import LoLDataset
+from src.data.dataset import LoLTrainDataset, LoLValidDataset
+import pdb
 
 class ChampionTransform(object):
     """ Resize and pad champion images to increase training size.
@@ -42,8 +43,8 @@ class ChampionTransform(object):
         transformed
             np.array, transformed image
         """
-        transformed = self.transform_image(image)
-        return transformed
+        transformed, box = self.transform_image(image)
+        return transformed, box
 
     def transform_image(self, image):
         """ Randomly add background noise to image by padding and resize
@@ -57,39 +58,42 @@ class ChampionTransform(object):
         y = random.randint(0, self.background.shape[0] - self.output_size - 1)
         x = random.randint(0, self.background.shape[1] - self.output_size - 1)
         base = self.background[y:y + self.output_size, x:x + self.output_size, :].copy()
-        if random.random() > 0.7:
-            base = cv2.flip(base, 1)
         
         new_size = random.randint(self.min_size, self.max_size)
-        scaled_image = cv2.resize(image,(new_size, new_size))
+        scaled_image = image.resize((new_size, new_size))
         
         pad = (self.output_size - new_size) // 2
         base[pad:pad + new_size, pad:pad + new_size, :] = scaled_image
+        box = [pad, pad, pad+new_size, pad+new_size]
             
-        return base
+        return base, box
 
 
-def get_train_dataloader(data_root, transform, train_val_split=0.8, batch_size=4, shuffle=True):
+def get_train_dataloader(data_root, background, output_size=65, object_size=(30, 40), batch_size=4, shuffle=True):
     
-    dataset = LoLDataset(data_root, test=False, transform=transform)
+    transform_train = ChampionTransform(background, output_size=output_size, object_size=object_size)
+
+    train_dataset = LoLTrainDataset(data_root, transform=transform_train)
     
-    # train, validation split
-    train_size = int(len(dataset) * train_val_split)
-    train_dataset, valid_dataset = random_split(dataset, [train_size, len(dataset)-train_size])
-    
-    # assign dataloaders
     train_dataloader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=shuffle)
-    valid_dataloader = DataLoader(dataset=valid_dataset, batch_size=batch_size, shuffle=shuffle)
+
+    trained_encoder = train_dataset.get_encoder()
     
-    return (train_dataloader, valid_dataloader)
+    return train_dataloader, trained_encoder
 
+def get_valid_dataloader(encoder, data_path='../screenshot.png', team='left', batch_size=4, shuffle=True):
 
+    valid_dataset = LoLValidDataset(encoder, data_path=data_path, team=team)
+
+    valid_dataloader = DataLoader(dataset=valid_dataset, batch_size=batch_size, shuffle=shuffle)
+
+    return valid_dataloader
+    
 if __name__ == "__main__":
     data_root = '~/Documents/falconai/Assets/Assets/FalconAIChallenge/champions/'
-    
-    game = cv2.imread(game_img_path, cv2.IMREAD_COLOR)
+    game_img_path = '~/Documents/falconai/Assets/Assets/FalconAIChallenge/screenshot.png'
+
+    game = Image.open(game_img_path).convert("RGB")
     background = game[90:-300, 80:-90, :]
 
-    transform_train = ChampionTransform(background, output_size=145, object_size=(30, 40))
-
-    train_dataloader, valid_dataloader = get_train_dataloader(data_root, transform_train)
+    train_dataloader = get_train_dataloader(data_root, background)
--- src/data/dataset.py
+++ src/data/dataset.py
@@ -1,15 +1,228 @@
 
 import os
+import numpy as np
+from PIL import Image
 import cv2
 import pandas as pd
 
 import torch
 import torchvision
 from torch.utils.data import Dataset
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
 
+import pdb
 
-class LoLDataset(Dataset):
+class LoLTrainDataset(Dataset):
+    """ LoL training dataset. """
+    
+    def __init__(self, data_root, transform=None) -> None:
+        """
+        Initializes parameters.
+
+        Parameters
+        ----------
+        data_root
+            string, dataset folder
+        test
+            bool, true when test set is used
+        transform
+            callable, transformation to be applied
+
+        Returns
+        -------
+        None
+        """
+        self.data_root = data_root
+        self.transform = transform
+        #self.encoder = OneHotEncoder(handle_unknown='ignore')
+        self.encoder = LabelEncoder()
+        self.image_paths = []
+        
+        self._init_dataset()
+        
+    def __len__(self) -> int:
+        return len(self.image_paths)
+    
+    def __getitem__(self, idx):
+        """
+        Returns item at index idx.
+
+        Parameters
+        ----------
+        idx
+            int, sample index number
+
+        Returns
+        -------
+        image
+            tensor, image at idx
+        encode_label
+            tensor, integer label of image (between 0 and 147)
+        """
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        transform_tensor = torchvision.transforms.Compose([
+            torchvision.transforms.ToTensor()])
+
+        path, label = self.image_paths[idx]
+        image = Image.open(path).convert("RGB")
+        image, box = self.transform(image)
+        image = transform_tensor(image)
+        area = (box[3] - box[1]) * (box[2] - box[0])
+        encoded_label = self.encoder.transform(label)#.toarray()
+        
+        return (image, encoded_label)
+    
+    def _init_dataset(self):
+        """
+        Dataset initalizer. Looks into data_root folder and collects
+        unique champion names for encoding.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        None
+        """
+        champions = set()
+
+        for name in os.listdir(self.data_root):
+            label = name.split('.')[0]
+            champions.add(label)
+            self.image_paths += [(os.path.join(self.data_root, name), [[label]])]
+        
+        #self.encoder = self.encoder.fit(np.array(list(champions)).reshape(-1, 1))
+        self.encoder = self.encoder.fit(list(champions))
+
+    def get_encoder(self):
+        return self.encoder
+
+class LoLValidDataset(Dataset):
+    """ LoL validation dataset. """
+    
+    def __init__(self, encoder, data_path='../screenshot.png', team='left') -> None:
+        """
+        Initializes parameters.
+
+        Parameters
+        ----------
+        data_root
+            string, dataset folder
+        test
+            bool, true when test set is used
+        transform
+            callable, transformation to be applied
+
+        Returns
+        -------
+        None
+        """
+        self.encoder = encoder
+        self.team = team
+        
+        image = cv2.imread(data_path, cv2.IMREAD_COLOR)
+
+        team_left, team_right = self.split_game_image(image, adjustment=5)
+        if self.team == 'left':
+            self.labels = ['Ornn', 'RekSai', 'Syndra', 'Aphelios', 'Thresh']
+            self.images = self.split_into_players(team_left, num_players=5)
+        elif self.team == 'right':
+            self.images = self.split_into_players(team_right, num_players=5)
+            # TODO: fill names
+        else:
+            raise ValueError('Team values must be either left or right.')
+        
+    def __len__(self) -> int:
+        return len(self.images)
+    
+    def __getitem__(self, idx):
+        """
+        Returns item at index idx.
+
+        Parameters
+        ----------
+        idx
+            int, sample index number
+
+        Returns
+        -------
+        image
+            tensor, image at idx
+        encode_label
+            tensor, integer label of image (between 0 and 147)
+        """
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        transform_tensor = torchvision.transforms.Compose([
+            torchvision.transforms.ToTensor()])
+
+        image = self.images[idx]
+        image = transform_tensor(image)
+        encoded_label = self.encoder.transform([self.labels[idx]]).ravel()
+        
+        return (image, encoded_label)
+
+    def split_game_image(self, game, adjustment=5):
+        """
+        Find the leaderboard within the screenshot and
+        split it in half to isolate two teams.
+
+        Parameters
+        ----------
+        game
+            np.array, game screenshot
+        adjustment
+            int, adjustment pixels as image leans towards right
+
+        Returns
+        -------
+        team_left
+            np.array, left half of the screenshot
+        team_right
+            np.array, right half of the screenshot
+        """
+        mid = game.shape[1]//2 + adjustment
+        width = 50
+        team_left = game[850:, mid-width:mid, :].copy()
+        team_right = game[850:, mid:mid+width+1, :].copy()
+
+        return team_left, team_right
+
+    def split_into_players(self, team, num_players=5):
+        """
+        Isolate players from team image.
+
+        Parameters
+        ----------
+        team
+            np.array, split team image
+        num_players
+            int, number of players
+
+        Returns
+        -------
+        players
+            list(np.array), images of individual players
+        """
+        height = team.shape[0] // num_players
+        players = []
+        
+        for h in range(num_players):
+            player = team[h * height:(h + 1) * height, :, :].copy()
+            players.append(self.convert_to_pil_image(player))
+        
+        return players
+    def convert_to_pil_image(self, image):
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image = Image.fromarray(image)
+        return image
+
+'''
+class LoLDataset_ObjectDetection(Dataset):
     """ LoL dataset. """
     
     def __init__(self, data_root, test=False, transform=None) -> None:
@@ -38,22 +251,32 @@ class LoLDataset(Dataset):
         if torch.is_tensor(idx):
             idx = idx.tolist()
 
+        transform_tensor = torchvision.transforms.Compose([
+            torchvision.transforms.ToTensor()])
         if self.test:
             path = self.image_paths[idx]
-            image = cv2.imread(path, cv2.IMREAD_COLOR)
+            image = Image.open(path).convert("RGB")
             # TODO: pass screenshot and crop transform here
             return image
 
         path, label = self.image_paths[idx]
-        image = cv2.imread(path, cv2.IMREAD_COLOR)
-        image = self.transform(image)
+        image = Image.open(path).convert("RGB")
+        image, box = self.transform(image)
+        image = transform_tensor(image)
+        area = (box[3] - box[1]) * (box[2] - box[0])
         one_hot_label = self.encoder.transform(label).toarray()
         
-        return (image, one_hot_label)
+        target = {}
+        target['boxes'] = torch.as_tensor([box], dtype=torch.float32)
+        target['labels'] = torch.as_tensor([one_hot_label], dtype=torch.int64)
+        target['image_id'] = torch.tensor([idx])
+        target['area'] = [area]
+
+        return (image, target)
     
     def _init_dataset(self):
         champions = set()
-        
+
         for name in os.listdir(self.data_root):
             label = name.split('.')[0]
             champions.add(label)
@@ -63,3 +286,4 @@ class LoLDataset(Dataset):
                 self.image_paths += [(os.path.join(self.data_root, name), [[label]])]
         
         self.encoder = self.encoder.fit(np.array(list(champions)).reshape(-1, 1))
+'''
