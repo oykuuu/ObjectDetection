41934fc6f4b17ec00e9ca17d2087bc3718213454
Oyku <han.oyku@gmail.com>
2020-02-17 21:49:45 -0500

    pylinted

--- src/match_template.py
+++ src/match_template.py
@@ -2,22 +2,26 @@
 of the players in the two teams. Uses a computer vision approach.
 """
 
-import numpy as np
 import cv2
 import os
 import json
 import argparse
+import numpy as np
 
 from utils.utils import show
-import pdb
 
 class LoLChampionDetector(object):
     """ Resize and pad champion images to increase training size.
     """
-    
-    def __init__(self, game_img_path = '../screenshot.png', champion_folder = '../champions/', n_players=5):
+
+    def __init__(
+            self,
+            game_img_path="../screenshot.png",
+            champion_folder="../champions/",
+            n_players=5,
+    ):
         """ Establishes game image and champion images.
-        
+
         Parameters
         ----------
         game_img_path
@@ -29,13 +33,18 @@ class LoLChampionDetector(object):
         -------
         None
         """
-        
+
         self.game = cv2.imread(game_img_path, cv2.IMREAD_COLOR)
-        self.champion_paths = [os.path.join(champion_folder, ch_file) for ch_file in os.listdir(champion_folder)]
-        self.champion_names = [ch_file.split('.')[0] for ch_file in os.listdir(champion_folder)]
+        self.champion_paths = [
+            os.path.join(champion_folder, ch_file)
+            for ch_file in os.listdir(champion_folder)
+        ]
+        self.champion_names = [
+            ch_file.split(".")[0] for ch_file in os.listdir(champion_folder)
+        ]
         self.n_champions = len(self.champion_names)
         self.n_players = n_players
-        
+
     def split_game_image(self, adjustment=5, width=50):
         """
         Find the leaderboard within the screenshot and
@@ -55,9 +64,9 @@ class LoLChampionDetector(object):
         team_right
             np.array, right half of the screenshot
         """
-        mid = self.game.shape[1]//2 + adjustment
-        team_left = self.game[850:, mid-width:mid, :].copy()
-        team_right = self.game[850:, mid:mid+width+1, :].copy()
+        mid = self.game.shape[1] // 2 + adjustment
+        team_left = self.game[850:, mid - width : mid, :].copy()
+        team_right = self.game[850:, mid : mid + width + 1, :].copy()
 
         return team_left, team_right
 
@@ -83,12 +92,12 @@ class LoLChampionDetector(object):
             the perfect match.
         """
         champion = cv2.imread(champion_file, cv2.IMREAD_COLOR)
-        scaled_champion = cv2.resize(champion,(scaled_size, scaled_size))
+        scaled_champion = cv2.resize(champion, (scaled_size, scaled_size))
         match = cv2.matchTemplate(team, scaled_champion, cv2.TM_CCOEFF_NORMED)
         min_val, max_val, min_loc, top_left = cv2.minMaxLoc(match)
-        
+
         return max_val
-    
+
     def compare_all_champions(self, team, all_sizes):
         """
         Try template matching of all champions at different scalings of
@@ -110,12 +119,12 @@ class LoLChampionDetector(object):
         """
         predictions = {}
         for new_size in all_sizes:
-            key_name = 'size_' + str(new_size)
+            key_name = "size_" + str(new_size)
             pred_fixed_size = []
             for champion_file in self.champion_paths:
                 max_val = self.match_champion(champion_file, team, new_size)
                 pred_fixed_size += [max_val]
-                
+
             predictions[key_name] = [self.champion_names, pred_fixed_size]
 
         return predictions
@@ -141,12 +150,14 @@ class LoLChampionDetector(object):
 
         for key_name, _ in predictions.items():
             top_match_index = np.array(predictions[key_name][1]).argsort()[-self.n_champions:][::-1]
-            top_match_champions = [predictions[key_name][0][id] for id in top_match_index]
+            top_match_champions = [
+                predictions[key_name][0][id] for id in top_match_index
+            ]
             top_match_vals = [predictions[key_name][1][id] for id in top_match_index]
-            
-            if np.mean(top_match_vals[:self.n_players]) > best_size_val:
-                best_size_val = np.mean(top_match_vals[:self.n_players])
-                best_size = int(key_name.split('_')[-1])
+
+            if np.mean(top_match_vals[: self.n_players]) > best_size_val:
+                best_size_val = np.mean(top_match_vals[: self.n_players])
+                best_size = int(key_name.split("_")[-1])
 
         return best_size
 
@@ -164,19 +175,24 @@ class LoLChampionDetector(object):
         best_size
             int, size of the template that gave the best overall match scores.
         """
-        key_name = 'size_' + str(size)
-        top_match_index = np.array(predictions[key_name][1]).argsort()[-self.n_players:][::-1]
-        top_match_champion_names = [predictions[key_name][0][id] for id in top_match_index]
+        key_name = "size_" + str(size)
+        top_match_index = np.array(predictions[key_name][1]).argsort()[
+            -self.n_players :
+        ][::-1]
+        top_match_champion_names = [
+            predictions[key_name][0][id] for id in top_match_index
+        ]
         top_match_vals = [predictions[key_name][1][id] for id in top_match_index]
-        predicted_players = top_match_champion_names[:self.n_players]
+        predicted_players = top_match_champion_names[: self.n_players]
         return predicted_players, top_match_champion_names, top_match_vals
+
 
 def main(config_path):
     config = json.load(open(config_path, "r"))
-    game_img_path = config['paths']['game_image_path']
-    champion_folder = config['paths']['champion_folder']
-    min_template_size = config['cv']['min_template_size']
-    max_template_size = config['cv']['max_template_size']
+    game_img_path = config["paths"]["game_image_path"]
+    champion_folder = config["paths"]["champion_folder"]
+    min_template_size = config["cv"]["min_template_size"]
+    max_template_size = config["cv"]["max_template_size"]
 
     all_sizes = np.arange(min_template_size, max_template_size + 1)
 
@@ -186,16 +202,23 @@ def main(config_path):
     right_predictions = lol_object.compare_all_champions(team_right, all_sizes)
 
     best_size = lol_object.find_best_scale(left_predictions)
-    print('Detected best size to scale champions as {} pixels.\n'.format(best_size))
-    
-    left_players, ordered_champions, ordered_vals = lol_object.get_matched_players(left_predictions, best_size)
-    right_players, ordered_champions, ordered_vals = lol_object.get_matched_players(right_predictions, best_size)
-    print('Left team members are predicted as: \n {}\n'.format(left_players))
-    print('Right team members are predicted as: \n {}\n'.format(right_players))
+    print("Detected best size to scale champions as {} pixels.\n".format(best_size))
+
+    left_players, ordered_champions, ordered_vals = lol_object.get_matched_players(
+        left_predictions, best_size
+    )
+    right_players, ordered_champions, ordered_vals = lol_object.get_matched_players(
+        right_predictions, best_size
+    )
+    print("Left team members are predicted as: \n {}\n".format(left_players))
+    print("Right team members are predicted as: \n {}\n".format(right_players))
+
 
 if __name__ == "__main__":
     PARSER = argparse.ArgumentParser()
-    PARSER.add_argument("-c", "--config", help="filepath to config json", default="./config.json")
+    PARSER.add_argument(
+        "-c", "--config", help="filepath to config json", default="./config.json"
+    )
     ARGS = PARSER.parse_args()
     CONFIGPATH = ARGS.config
     main(CONFIGPATH)
