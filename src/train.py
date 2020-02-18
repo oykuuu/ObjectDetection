cfa5bd821e154e76642a88de6f96cac6deb4a087
Oyku <han.oyku@gmail.com>
2020-02-17 21:46:44 -0500

    config variables updated

--- src/train.py
+++ src/train.py
@@ -4,24 +4,21 @@ of the players in the two teams. Uses a computer vision approach.
 
 import os
 import json
-import numpy as np
 import copy
 import time
 import argparse
+import numpy as np
 import cv2
 
-import torchvision
 from torchvision import models
 import torch
 import torch.nn as nn
 import torch.optim as optim
 
-from data.dataset import LoLTrainDataset, LoLValidDataset
-from data.dataloader import get_train_dataloader, get_valid_dataloader, ChampionTransform
+from data.dataloader import get_train_dataloader, get_valid_dataloader
 
-import pdb
 
-def train_model(model, dataloaders, criterion, optimizer, num_epochs=10, device='cpu'):
+def train_model(model, dataloaders, criterion, optimizer, num_epochs=10, device="cpu"):
     """
     Trains the Deep Learning model. Keeps track of the best model according to the
     holdout validation set.
@@ -49,61 +46,66 @@ def train_model(model, dataloaders, criterion, optimizer, num_epochs=10, device=
         list, list of validation accuracy at different timesteps
     """
     since = time.time()
-    
+
     val_acc_history = []
     best_model_params = copy.deepcopy(model.state_dict())
     best_acc = 0.0
-    
+
     for epoch in range(num_epochs):
-        print('\nEpoch {}/{}'.format(epoch + 1, num_epochs))
-        print('----------')
-        
-        for phase in ['train', 'valid']:
-            if phase == 'train':
+        print("\nEpoch {}/{}".format(epoch + 1, num_epochs))
+        print("----------")
+
+        for phase in ["train", "valid"]:
+            if phase == "train":
                 model.train()
             else:
                 model.eval()
-            
+
             running_loss = 0.0
             running_corrects = 0
-            
+
             for inputs, labels in dataloaders[phase]:
                 inputs = inputs.to(device)
                 labels = labels.to(device)
-                
+
                 optimizer.zero_grad()
-                with torch.set_grad_enabled(phase == 'train'):
+                with torch.set_grad_enabled(phase == "train"):
                     outputs = model(inputs)
                     loss = criterion(outputs, labels.squeeze(1))
                     _, preds = torch.max(outputs, 1)
-                    
-                    if phase == 'train':
+
+                    if phase == "train":
                         loss.backward()
                         optimizer.step()
-                
+
                 running_loss += loss.item() * inputs.size(0)
                 running_corrects += torch.sum(preds == labels.data)
                 epoch_loss = running_loss / len(dataloaders[phase].dataset)
                 epoch_acc = running_corrects.double() / len(dataloaders[phase].dataset)
-                
-                
-                
-                if phase == 'valid':
+
+                if phase == "valid":
                     val_acc_history.append(epoch_acc)
                     if epoch_acc > best_acc:
                         best_acc = epoch_acc
                         best_model_params = copy.deepcopy(model.state_dict())
-            print('{} - Loss: {:.4f} \t Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
+            print(
+                "{} - Loss: {:.4f} \t Acc: {:.4f}".format(phase, epoch_loss, epoch_acc)
+            )
     time_elapsed = time.time() - since
-    
-    print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
-    print('Best validation accuracy: {:.4f}\n\n'.format(best_acc))
-    print('-' * 10)
-    
+
+    print(
+        "Training complete in {:.0f}m {:.0f}s".format(
+            time_elapsed // 60, time_elapsed % 60
+        )
+    )
+    print("Best validation accuracy: {:.4f}\n\n".format(best_acc))
+    print("-" * 10)
+
     model.load_state_dict(best_model_params)
     return model, val_acc_history
 
-def get_resnet(num_classes, device='cpu'):
+
+def get_resnet(num_classes, device="cpu"):
     """
     Obtain the pretrained Resnet-18 model and allow for the training of
     the last layer only.
@@ -126,12 +128,15 @@ def get_resnet(num_classes, device='cpu'):
     for param in model.parameters():
         param.requires_grad = False
 
-    model.fc = nn.Linear(512, num_classes) # by default requires_grad=True
+    model.fc = nn.Linear(512, num_classes)  # by default requires_grad=True
     model = model.to(device)
-    
-    params_to_update = [param for name, param in model.named_parameters() if param.requires_grad]
-            
+
+    params_to_update = [
+        param for name, param in model.named_parameters() if param.requires_grad
+    ]
+
     return model, params_to_update
+
 
 def get_team_predictions(model, team_dataloader, encoder):
     """
@@ -153,7 +158,7 @@ def get_team_predictions(model, team_dataloader, encoder):
     """
     predictions = []
     model.eval()
-    for inputs, labels in team_dataloader:
+    for inputs, _ in team_dataloader:
         preds = nn.LogSoftmax(dim=1)(model(inputs)).detach().numpy()
         pred = np.argmax(preds)
         predictions.append(pred)
@@ -161,57 +166,77 @@ def get_team_predictions(model, team_dataloader, encoder):
     names_predicted = encoder.inverse_transform(predictions)
     return list(names_predicted)
 
+
 def main(config_path):
     config = json.load(open(config_path, "r"))
-    game_img_path = config['paths']['game_image_path']
-    champion_folder = config['paths']['champion_folder']
+    game_img_path = config["paths"]["game_image_path"]
+    champion_folder = config["paths"]["champion_folder"]
 
-    random_seed = config['dl']['random_seed']
     num_classes = len(os.listdir(champion_folder))
-    num_epochs = config['dl']['epochs']
-    learning_rate = config['dl']['learning_rate']
-    momentum = config['dl']['momentum']
-    batch_size = config['dl']['batch_size']
+    num_epochs = config["dl"]["epochs"]
+    learning_rate = config["dl"]["learning_rate"]
+    momentum = config["dl"]["momentum"]
+    batch_size = config["dl"]["batch_size"]
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    
+
     # crop the background image from the game screenshot
     game = cv2.imread(game_img_path, cv2.IMREAD_COLOR)
     background = game[90:-300, 80:-90, :]
 
     # get dataloaders
-    train_dataloader, encoder = get_train_dataloader(champion_folder, background, output_size=65, object_size=(35, 45), batch_size=batch_size, shuffle=True)
-    valid_dataloader, _ = get_valid_dataloader(encoder, data_path=game_img_path, team='right', batch_size=batch_size, shuffle=True)
+    train_dataloader, encoder = get_train_dataloader(
+        champion_folder,
+        background,
+        output_size=65,
+        object_size=(35, 45),
+        batch_size=batch_size,
+        shuffle=True,
+    )
+    valid_dataloader, _ = get_valid_dataloader(
+        encoder,
+        data_path=game_img_path,
+        team="right",
+        batch_size=batch_size,
+        shuffle=True,
+    )
 
     dataloaders = {}
-    dataloaders['train'] = train_dataloader
-    dataloaders['valid'] = valid_dataloader
+    dataloaders["train"] = train_dataloader
+    dataloaders["valid"] = valid_dataloader
 
     # get Resnet-18 model
     model, params_to_update = get_resnet(num_classes, device)
-    #optimizer = optim.SGD(params_to_update, lr=learning_rate, momentum=momentum)
+    # optimizer = optim.SGD(params_to_update, lr=learning_rate, momentum=momentum)
     optimizer = optim.Adam(params_to_update, lr=learning_rate)
     criterion = nn.CrossEntropyLoss()
 
     # train model
-    model, val_hist = train_model(model, dataloaders, criterion, optimizer, num_epochs=num_epochs, device=device)
+    model, _ = train_model(
+        model, dataloaders, criterion, optimizer, num_epochs=num_epochs, device=device
+    )
 
     # get team predictions
-    left_dataloader, left_true_labels = get_valid_dataloader(encoder, data_path=game_img_path, team='left', batch_size=1, shuffle=False)
-    right_dataloader, right_true_labels = get_valid_dataloader(encoder, data_path=game_img_path, team='right', batch_size=1, shuffle=False)
+    left_dataloader, left_true_labels = get_valid_dataloader(
+        encoder, data_path=game_img_path, team="left", batch_size=1, shuffle=False
+    )
+    right_dataloader, right_true_labels = get_valid_dataloader(
+        encoder, data_path=game_img_path, team="right", batch_size=1, shuffle=False
+    )
     left_players = get_team_predictions(model, left_dataloader, encoder)
     right_players = get_team_predictions(model, right_dataloader, encoder)
-    
-    print('Left team members are predicted as: \n {}\n'.format(left_players))
-    print('Actual feft team members are: \n {}\n\n'.format(left_true_labels))
-    print('Right team members are predicted as: \n {}\n'.format(right_players))
-    print('Actual right team members are: \n {}\n'.format(right_true_labels))
+
+    print("Left team members are predicted as: \n {}\n".format(left_players))
+    print("Actual feft team members are: \n {}\n\n".format(left_true_labels))
+    print("Right team members are predicted as: \n {}\n".format(right_players))
+    print("Actual right team members are: \n {}\n".format(right_true_labels))
 
 
 if __name__ == "__main__":
     PARSER = argparse.ArgumentParser()
-    PARSER.add_argument("-c", "--config", help="filepath to config json", default="./config.json")
+    PARSER.add_argument(
+        "-c", "--config", help="filepath to config json", default="./config.json"
+    )
     ARGS = PARSER.parse_args()
     CONFIGPATH = ARGS.config
     main(CONFIGPATH)
-
